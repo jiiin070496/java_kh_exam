8570b8c51d7a921087ccbc320bf5141eb744918a
jiiin070496 <jin03154@naver.com>
2023-06-13 01:36:21 +0900

    PhoneControl

---
+++ com.kh.practice.chap02.model.vo/src/com/kh/practice/chap02/run/Run.java
@@ -0,0 +1,12 @@
+package com.kh.practice.chap02.run;
+
+import com.kh.practice.chap02_abstractNInterface.PhoneController;
+
+public class Run {
+
+	public static void main(String[] args) {
+		PhoneController pc = new PhoneController();
+		
+	}
+
+}
---
+++ com.kh.practice.chap02.model.vo/src/com/kh/practice/chap02_abstractNInterface/PhoneController.java
@@ -0,0 +1,26 @@
+package com.kh.practice.chap02_abstractNInterface;
+
+import com.kh.practice.chap02.model.vo.GalaxyNote9;
+import com.kh.practice.chap02.model.vo.Phone;
+import com.kh.practice.chap02.model.vo.SmartPhone;
+import com.kh.practice.chap02.model.vo.V40;
+
+public class PhoneController {
+
+	public String[] result = new String[2];
+	public String[] method() {
+		SmartPhone[] phone = new SmartPhone[2];
+		phone[0] = new GalaxyNote9();
+		phone[1] = new V40();
+		for(int i = 0; i<2; i++) {
+			if(phone[i] instanceof GalaxyNote9) {
+				result[i] = phone[i].printInformation();
+			}else if(phone[i] instanceof V40) {
+				result[i] = phone[i].printInformation();
+			}
+		}
+		return result;
+	}
+}
+
+
